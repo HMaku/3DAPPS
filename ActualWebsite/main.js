b98e55db681ef5a2c347f003566086b0e4b46920
Yoame <65726618+HMaku@users.noreply.github.com>
2025-02-17 17:38:26 +0000

    Lab 5

--- ActualWebsite/main.js
+++ ActualWebsite/main.js
@@ -7,8 +7,8 @@ function init(){
 	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 	
-	camera.position.x = 5;
-	camera.position.z = -3;
+	//camera.position.x = 5;
+	camera.position.z = -5;
 	camera.lookAt(0, 0, 0);
 	renderer = new THREE.WebGLRenderer();
 	renderer.setSize( window.innerWidth, window.innerHeight );
