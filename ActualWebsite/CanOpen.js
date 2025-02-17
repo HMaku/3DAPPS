cdf4b2f9c79446b238e09eee3c1cc5b50cf8da97
Yoame <65726618+HMaku@users.noreply.github.com>
2025-02-17 20:25:27 +0000

    ????

---
+++ ActualWebsite/CanOpen.js
@@ -0,0 +1,99 @@
+const { update } = require("three/examples/jsm/libs/tween.module.js");
+
+var scene, camera, renderer, action = [];
+
+init();
+
+function init(){
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+	
+	//camera.position.x = 5;
+	camera.position.z = -5;
+    camera.position.y = 4
+	camera.lookAt(0, 0, 0);
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize( window.innerWidth, window.innerHeight );
+	renderer.setAnimationLoop( animate );
+	document.body.appendChild( renderer.domElement );
+
+	let light
+
+	light = new THREE.DirectionalLight(); 
+	light.position.set(5, -11, -9);
+	scene.add(light);
+	
+	light = new THREE.DirectionalLight(); 
+	light.position.set(-17, -11, -12);
+	scene.add(light);
+
+	light = new THREE.DirectionalLight(); 
+	light.position.set(12, -9, 16);
+	scene.add(light);
+
+	light = new THREE.DirectionalLight(); 
+	light.position.set(2, 10, -0.5);
+	scene.add(light);
+
+	light = new THREE.DirectionalLight(); 
+	light.position.set(8, 10, -14);
+	scene.add(light);
+
+	
+	const loader = new THREE.GLTFLoader();
+	loader.load("Assets/OpenCan.glb", function (gltf) {
+		model = gltf.scene;
+		scene.add(model);
+		//animate();
+	});
+
+    const mixer = new THREE.AnimationMixer(model);
+    const animations = gltf.animations;
+    animations.forEach((clip) => {
+    const action = mixer.clipAction(clip);
+    actions.push(action)
+        
+      });
+
+	window.addEventListener('resize', onResize, false);
+    animate();
+
+}
+
+function animate() {
+	requestAnimationFrame(animate);
+	if(mixer)
+	{
+		mixer.update(clock.getDelta());
+	}
+	
+	renderer.render(scene,camera);
+    update();
+}
+
+function onResize(){
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+dropDown();
+function dropDown()
+{
+	document.addEventListener("DOMContentLoaded", function () {
+		let dropdowns = document.querySelectorAll(".dropdown");
+
+		dropdowns.forEach(function (dropdown) {
+			dropdown.addEventListener("mouseover", function () {
+				let menu = this.querySelector(".dropdown-menu");
+				menu.classList.add("show");
+			});
+
+			dropdown.addEventListener("mouseleave", function () {
+				let menu = this.querySelector(".dropdown-menu");
+				menu.classList.remove("show");
+			});
+		});
+	});
+}
